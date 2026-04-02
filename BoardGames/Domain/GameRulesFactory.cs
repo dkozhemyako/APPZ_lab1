f1430915c0743ab5207935c3d27b817298a1aacf
dk <d.kozhemyako@icloud.com>
2026-04-02 15:05:59 +0300

    Add GameRulesFactory for IGameRules

---
+++ BoardGames/Domain/GameRulesFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BoardGames.Domain;
+
+public static class GameRulesFactory
+{
+    public static IGameRules Create(GameType gameType)
+    {
+        switch (gameType)
+        {
+            case GameType.Chess:
+                return new ChessRules();
+            case GameType.Checkers:
+                return new CheckersRules();
+            case GameType.Backgammon:
+                return new BackgammonRules();
+            case GameType.Monopoly:
+                return new MonopolyRules();
+            default:
+                throw new ArgumentOutOfRangeException(nameof(gameType));
+        }
+    }
+}
